24059e0c0c95ebf987ddfbfe418e394548459819
Sadik Mohammad <sadiqrangrej10@gmail.com>
2026-07-16 17:03:20 +0530

    Add PoseEstimationMin.py for basic pose estimation functionality

---
+++ Computer_Vision/PoseEstimationProject/PoseEstimationMin.py
@@ -0,0 +1,21 @@
+import cv2
+import mediapipe as mp
+import time
+
+cap = cv2.VideoCapture('PoseVideos/1.mp4')
+pTime = 0
+while True:
+    success, img = cap.read()
+    cv2.imshow("Image", img)
+
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
+                (255, 0, 255), 3)
+    cv2.waitKey(1)
+
+
+
+
+    
